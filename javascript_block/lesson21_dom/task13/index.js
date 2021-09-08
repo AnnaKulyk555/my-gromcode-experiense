2ccf24b8cddddc725205cabad462d5827a65eb4c
annaKulyk <ankakulik1998@gmail.com>
2021-09-08 22:52:08 +0300

    DOM home task 12

---
+++ javascript_block/lesson21_dom/task13/index.js
@@ -0,0 +1,10 @@
+export function getSection(num) {
+  const spanElem = document.querySelector(`span[data-number="${num}"]`);
+
+  return spanElem.closest('.box').dataset.section;
+}
+
+console.log(getSection(3));
+console.log(getSection(6));
+console.log(getSection(1));
+console.log(getSection(5));
