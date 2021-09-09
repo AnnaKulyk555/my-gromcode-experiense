24ee117332f15133bb3ba7d314bc880f2f080a94
annaKulyk <ankakulik1998@gmail.com>
2021-09-09 22:42:43 +0300

    EVENTS home task 5

--- javascript_block/lesson22_events/task6/index.js
+++ javascript_block/lesson22_events/task6/index.js
@@ -1,25 +1,7 @@
 const btnElem = document.querySelector('.single-use-btn');
 
-// WORK
-btnElem.addEventListener('click', function btnElemHandler() {
-  btnElem.removeEventListener('click', btnElemHandler);
+const btnElemHandler = () => {
   console.log('clicked');
-});
-
-// DON'T WORK
-// const btnElemHandler = () => {
-//   btnElem.removeEventListener('click', btnElemHandler);
-
-//   console.log('clicked');
-// };
-
-// btnElem.addEventListener('click', () => btnElemHandler());
-
-// DON'T WORK
-// function btnElemHandler() {
-//   btnElem.removeEventListener('click', btnElemHandler);
-
-//   console.log('clicked');
-// }
-
-// btnElem.addEventListener('click', () => btnElemHandler());
+  btnElem.removeEventListener('click', btnElemHandler);
+};
+btnElem.addEventListener('click', btnElemHandler);
