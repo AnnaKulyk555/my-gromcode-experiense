fce69fd4a1a04295fe5d96bb129d2c76438e0b55
annaKulyk <ankakulik1998@gmail.com>
2021-09-09 18:37:10 +0300

    EVENTS home task 2

---
+++ javascript_block/lesson22_events/task3/index.js
@@ -0,0 +1,5 @@
+const textInput = document.querySelector('.text-input');
+const inputChangeHandler = () => {
+  console.log(textInput.value);
+};
+textInput.addEventListener('change', () => inputChangeHandler());
