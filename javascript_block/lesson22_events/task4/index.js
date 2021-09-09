2a90d78c3e0da52f34f856d9ba79d3d373d6ca54
annaKulyk <ankakulik1998@gmail.com>
2021-09-09 23:19:28 +0300

    home task 3

--- javascript_block/lesson22_events/task4/index.js
+++ javascript_block/lesson22_events/task4/index.js
@@ -2,4 +2,4 @@ const checkboxElem = document.querySelector('.task-status');
 const checkboxHandler = () => {
   console.log(checkboxElem.checked);
 };
-checkboxElem.addEventListener('change', () => checkboxHandler());
+checkboxElem.addEventListener('change', checkboxHandler);
