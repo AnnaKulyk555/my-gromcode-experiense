7218bfb7d28e07e528433d0db231ecaa3da26aa1
annaKulyk <ankakulik1998@gmail.com>
2021-09-13 15:43:32 +0300

    EVENTS to-do home work

---
+++ javascript_block/lesson23_events/task3/index.js
@@ -0,0 +1,76 @@
+const listElem = document.querySelector('.list');
+
+const tasks = [
+  { text: 'Buy milk', done: false },
+  { text: 'Pick up Tom from airport', done: false },
+  { text: 'Visit party', done: false },
+  { text: 'Visit doctor', done: true },
+  { text: 'Buy meat', done: true },
+];
+
+const renderTasks = tasksList => {
+  listElem.innerHTML = '';
+
+  const tasksElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done }, index) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      const checkbox = document.createElement('input');
+
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.setAttribute('data-id', index);
+      checkbox.checked = done;
+
+      checkbox.classList.add('list__item-checkbox');
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+
+      listItemElem.append(checkbox, text);
+
+      return listItemElem;
+    });
+
+  listElem.append(...tasksElems);
+};
+
+renderTasks(tasks);
+
+const createBtnElement = document.querySelector('.create-task-btn');
+const inputElement = document.querySelector('.task-input');
+
+function onCreateTask(event) {
+  const { value } = inputElement;
+  tasks.unshift({
+    text: value,
+    done: false,
+  });
+
+  renderTasks(tasks);
+  inputElement.value = '';
+}
+
+createBtnElement.addEventListener('click', onCreateTask);
+
+function updateTaskHandler(event) {
+  // console.log(tasks);
+
+  if (!event.target.classList.contains('list__item-checkbox')) {
+    return;
+  }
+
+  // console.log('checbox is clicked');
+  const selectedChecboxId = event.target.dataset.id;
+  if (tasks[selectedChecboxId].done === true) {
+    tasks[selectedChecboxId].done = false;
+  } else {
+    tasks[selectedChecboxId].done = true;
+  }
+
+  // console.log(selectedChecboxId);
+  // console.log(tasks);
+  renderTasks(tasks);
+}
+
+listElem.addEventListener('click', updateTaskHandler);
