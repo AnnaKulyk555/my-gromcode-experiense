fc04fb680ebc39ce27a82c3ade67c1433f4102e0
annaKulyk <ankakulik1998@gmail.com>
2021-09-14 18:48:01 +0300

    DATE task 2

---
+++ javascript_block/lesson24_dates/task2/index.js
@@ -0,0 +1,11 @@
+const formatter = new Intl.DateTimeFormat('en', {
+  timeZone: 'UTC',
+  hour: '2-digit',
+  minute: '2-digit',
+  hour12: false,
+});
+
+export const getGreenWichTime = date => formatter.format(date);
+
+// test data
+console.log(getGreenWichTime(new Date()));
