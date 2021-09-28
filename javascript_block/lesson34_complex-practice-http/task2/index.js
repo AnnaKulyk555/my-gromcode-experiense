a9412e461837707bc0d4c89af0aa0a7dd621379c
annaKulyk <ankakulik1998@gmail.com>
2021-09-28 19:08:52 +0300

    create user refactoring

--- javascript_block/lesson34_complex-practice-http/task2/index.js
+++ javascript_block/lesson34_complex-practice-http/task2/index.js
@@ -5,7 +5,20 @@ const emailInputElem = document.querySelector('[name="email"]');
 const nameInputElem = document.querySelector('[name="name"]');
 const passwordInputElem = document.querySelector('[name="password"]');
 
-submitBtn.removeAttribute('disabled');
+const onInputChange = () => {
+  const isValidForm = formElem.reportValidity();
+  if (isValidForm) {
+    submitBtn.removeAttribute('disabled');
+    errorElem.textContent = '';
+  } else {
+    submitBtn.setAttribute('disabled', true);
+    errorElem.textContent = '';
+  }
+};
+
+emailInputElem.addEventListener('input', onInputChange);
+nameInputElem.addEventListener('input', onInputChange);
+passwordInputElem.addEventListener('input', onInputChange);
 
 const createUser = formData =>
   fetch('https://6151af974a5f22001701d380.mockapi.io/p1/user', {
@@ -14,22 +27,20 @@ const createUser = formData =>
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(formData),
-  }).then(response => response.json());
+  }).then(response => (response.ok ? response.json() : Promise.reject(response)));
 
 const onFormSubmit = event => {
   event.preventDefault();
-  if (formElem.reportValidity()) {
-    console.log(true);
-  }
 
   const formData = Object.fromEntries(new FormData(formElem));
-  createUser(formData).then(result => {
-    alert(JSON.stringify(result));
-    emailInputElem.value = '';
-    nameInputElem.value = '';
-    passwordInputElem.value = '';
-  });
+  createUser(formData)
+    .then(result => {
+      alert(JSON.stringify(result));
+      emailInputElem.value = '';
+      nameInputElem.value = '';
+      passwordInputElem.value = '';
+    })
+    .catch(() => (errorElem.textContent = 'Failed to create user'));
 };
 
 formElem.addEventListener('submit', onFormSubmit, false);
-// errorElem.append('Failed to create user')
